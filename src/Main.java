14988a32dfafd93432276bb8193f3578c13c30f5
enoshindupalli <indupallienosh2023@gmail.com>
2025-11-19 22:19:29 -0600

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int size = 10_000;
+        List<Integer> numb = new ArrayList<>(size);
+
+
+        Random fun = new Random();
+
+        for(int i = 0; i < size; i++){
+
+            numb.add(fun.nextInt(100));
+
+        }
+
+        long startSeq = System.currentTimeMillis();
+        int sum1 = numb.stream()
+                .map(n -> n*2)
+                .mapToInt(n -> n)
+                .sum();
+        long endSeq = System.currentTimeMillis();
+
+
+        long startPara = System.currentTimeMillis();
+        int sum2 = numb.parallelStream()
+                .map(n -> n*2)
+                .mapToInt(n -> n)
+                .sum();
+        long endPara = System.currentTimeMillis();
+
+        System.out.println(sum1 + " : " + (endSeq - startSeq));
+        System.out.println(sum2 + " : " + (endPara - startPara));
+
+
+
+    }
+}
